d7d2ee1e05ac914210b3e8aaaced2654382eb76e
Sara-FH <faerch23@gmail.com>
2020-08-13 15:32:52 +0200

    Trying to get rid off spaces in between genus and species + getting unique specieslist to find a and b values from fishbase.

--- Github_UVC_DOVS.R
+++ Github_UVC_DOVS.R
@@ -17,9 +17,22 @@ DOVS$X <- NULL
 library(dplyr)
 library(stringr)
 DOVS %>%
-  mutate(across(where(is.character), str_trim))
+  mutate(across(where(is.character), str_trim)) # I am not sure this is working, it seems like there are still
+                                                # too many spaces between some of the genus and species names
 #Combine Genus and Species into SPECIES for DOVS data
 DOVS$SPECIES <- paste(DOVS$Genus , " ", DOVS$Species)
 
 #Rename DOVS Number_individuals to N
 names(DOVS)[names(DOVS) == "Number_individuals"] <- "N"
+
+#Getting unique species for finding a and b factors to calculate biomass
+library(data.table)
+unique_dovs <- data.frame(unique(DOVS$SPECIES))# I am not sure why one of the unique values is empty ??
+colnames(unique_dovs) <- "species"
+unique_uvc <- data.frame(unique(UVC$SPECIES))   # This one does not have an empty value..
+colnames(unique_uvc) <- "species"
+unique_species <- unique(rbind(unique_dovs, unique_uvc))
+
+
+#Calculating fish biomass
+
