de18832ab5b545042d4542bb15719436008faab1
Denisse Fierro Arcos <lidefi87@hotmail.com>
2020-08-13 10:58:10 -0500

    Editing code to unite species name into one column - DFA

--- Github_UVC_DOVS.R
+++ Github_UVC_DOVS.R
@@ -1,30 +1,102 @@
+#I have added this section because it can be used to give a nice overview of what the
+#script does and you can use it as a way to show that you know how to code when you
+#are applying for a job or any academic program
+#You can delete this is you do not like it, or do not plan to share it.
+
+#############################################################
+# Title: Github data cleaning and analysis UVC and DOVS Galapagos
+# Author: Sara Hansen
+# Assisting: Denisse Fierro Arcos
+# Version: 1
+# Date last updated: 2020-08-13
+# Aim: Explain what the code does, maybe what project is related to (e.g., Master thesis)
+#############################################################
+
 #Github data cleaning and analysis UVC and DOVS Galapagos
 #############################################################
-#Set working directory and Load UVC and DOVS data
-setwd("C:/Users/Ejer/Desktop/Github_UVC_DOVS/Data")
-UVC <- read.csv(file = "UVC_all_clean.csv", header = TRUE, stringsAsFactors = FALSE)
-DOVS <- read.csv(file = "DOVS_clean.csv", header = TRUE, stringsAsFactors = FALSE)
 
-#Removing X column for DOVS and UVC
-UVC$X <- NULL
-DOVS$X <- NULL
+# Uploading data ----------------------------------------------------------
+#Set working directory and Load UVC and DOVS data
+# setwd("C:/Users/Ejer/Desktop/Github_UVC_DOVS/Data") # I have removed this because you
+#have created a project, so only relative paths are needed I have edit below
+UVC <- read.csv(file = "Data/UVC_all_clean.csv", header = TRUE, 
+                #This number in the square brackets removes the first column
+                stringsAsFactors = FALSE)[,-1] 
+DOVS <- read.csv(file = "Data/DOVS_clean.csv", header = TRUE, 
+                 stringsAsFactors = FALSE)[,-1]
+
+#I have removed them above now
+# #Removing X column for DOVS and UVC
+# UVC$X <- NULL
+# DOVS$X <- NULL
 
 #Removing several columns - DOVs: Comments, Stage. UVC: Comments, Sex
 #DOVS <- DOVS[, -c("Comments", "Stage")]
 #UVC <- UVC[, -c("Comments", "Sex")]
 
+# Uploading libraries -----------------------------------------------------
 #Remove spaces around text (characters) in DOVS, using mutate function from dplyr and trim from stringr
-library(dplyr)
-library(stringr)
+# library(dplyr)
+# library(stringr)
+library(tidyverse) #this one contains a bunch of useful packages including the two above
+
+
+# Tidying up dataset ------------------------------------------------------
 DOVS %>%
+  #First we will check unique values for Family, Genus and Species
+  distinct(Family, Genus, Species) %>% 
+  arrange(Family, Genus)
+#Results suggest that there are a number of Genus with an additional blank space at the
+#end. We also have a blank row at the top, which are for unknown species. Check with
+#Pelayo if we know what they could be, but otherwise we must remove them
+
+#This takes out white space before or after a word in all columns that are of class
+#character. However, because you did not save it, when you merge the columns together
+#you still had the spaces. I have included below how to save the outcome, but I have
+#rewritten this section so everything appears in just one chunk of code
+DOVS_S <- DOVS %>% 
   mutate(across(where(is.character), str_trim)) # I am not sure this is working, it seems like there are still
-                                                # too many spaces between some of the genus and species names
+# too many spaces between some of the genus and species names
+
 #Combine Genus and Species into SPECIES for DOVS data
-DOVS$SPECIES <- paste(DOVS$Genus , " ", DOVS$Species)
+DOVS_S$SPECIES <- paste(DOVS$Genus , " ", DOVS$Species) #This includes two spaces because
+#the default for paste is to have " " as a separator. Try this instead:
+DOVS_S$SPECIES <- paste(DOVS_S$Genus, DOVS_S$Species)
 
 #Rename DOVS Number_individuals to N
-names(DOVS)[names(DOVS) == "Number_individuals"] <- "N"
+names(DOVS_S)[names(DOVS_S) == "Number_individuals"] <- "N"
 
+
+#Tidying up data - I have done the same as above and saved it as a different data frame
+#so you can compare and see which one you would like to keep
+DOVS_D <- DOVS %>% 
+  #removing columns as you have commented out above
+  select(-c(Comment, Method)) %>% 
+  #Removing empty rows in Family 
+  filter(Family != "") %>% 
+  #Rename Number_individuals column
+  rename("N" = "Number_individuals") %>% 
+  #Removing spaces in Genus column
+  mutate(Genus = str_trim(Genus, side = "both"),
+         #I also want the empty Species rows to now include sp
+         Species = case_when(Species == "" ~ "sp",
+                             #This means that everything else remains the same
+                             TRUE ~ Species)) %>% 
+  #Now we create a new column for the combined species name
+  #If Genus is not empty, our new column will join Genus and Species
+  mutate(SpeciesName = case_when(Genus != "" ~ paste(Genus, Species, sep = " "),
+                                   #If Genus is empty, then join Species and Family
+                                   Genus == "" ~ paste(Family, Species, sep = " ")))
+#We can check our progress
+DOVS_D %>% 
+  distinct(Family, Genus, Species) %>% 
+  arrange(Family, Genus)
+#Everything looks ok, except the Mycteroperca sp. From memory M. olfax was the only
+#species in this genus in the Galapagos. Check with Pelayo and change if needed.
+
+
+
+# Biomass calculations ----------------------------------------------------
 #Getting unique species for finding a and b factors to calculate biomass
 library(data.table)
 unique_dovs <- data.frame(unique(DOVS$SPECIES))# I am not sure why one of the unique values is empty ??
