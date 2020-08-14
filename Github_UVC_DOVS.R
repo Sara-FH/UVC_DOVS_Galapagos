26f9224d6cec66e40962aa4e6a20cf8f8472af3c
Sara-FH <faerch23@gmail.com>
2020-08-14 14:53:37 +0200

    Made unique specieslist and corrected wrong species names. Working on NMDS plot for abundance.

--- Github_UVC_DOVS.R
+++ Github_UVC_DOVS.R
@@ -1,11 +1,6 @@
-#I have added this section because it can be used to give a nice overview of what the
-#script does and you can use it as a way to show that you know how to code when you
-#are applying for a job or any academic program
-#You can delete this is you do not like it, or do not plan to share it.
-
 #############################################################
 # Title: Github data cleaning and analysis UVC and DOVS Galapagos
-# Author: Sara Hansen
+# Author: Sara Færch Hansen
 # Assisting: Denisse Fierro Arcos
 # Version: 1
 # Date last updated: 2020-08-13
@@ -25,11 +20,6 @@ UVC <- read.csv(file = "Data/UVC_all_clean.csv", header = TRUE,
 DOVS <- read.csv(file = "Data/DOVS_clean.csv", header = TRUE, 
                  stringsAsFactors = FALSE)[,-1]
 
-#I have removed them above now
-# #Removing X column for DOVS and UVC
-# UVC$X <- NULL
-# DOVS$X <- NULL
-
 #Removing several columns - DOVs: Comments, Stage. UVC: Comments, Sex
 #DOVS <- DOVS[, -c("Comments", "Stage")]
 #UVC <- UVC[, -c("Comments", "Sex")]
@@ -41,7 +31,7 @@ DOVS <- read.csv(file = "Data/DOVS_clean.csv", header = TRUE,
 library(tidyverse) #this one contains a bunch of useful packages including the two above
 
 
-# Tidying up dataset ------------------------------------------------------
+# Tidying up data set ------------------------------------------------------
 DOVS %>%
   #First we will check unique values for Family, Genus and Species
   distinct(Family, Genus, Species) %>% 
@@ -55,11 +45,10 @@ DOVS %>%
 #you still had the spaces. I have included below how to save the outcome, but I have
 #rewritten this section so everything appears in just one chunk of code
 DOVS_S <- DOVS %>% 
-  mutate(across(where(is.character), str_trim)) # I am not sure this is working, it seems like there are still
-# too many spaces between some of the genus and species names
+  mutate(across(where(is.character), str_trim))
 
 #Combine Genus and Species into SPECIES for DOVS data
-DOVS_S$SPECIES <- paste(DOVS$Genus , " ", DOVS$Species) #This includes two spaces because
+#DOVS_S$SPECIES <- paste(DOVS$Genus , " ", DOVS$Species) #This includes two spaces because
 #the default for paste is to have " " as a separator. Try this instead:
 DOVS_S$SPECIES <- paste(DOVS_S$Genus, DOVS_S$Species)
 
@@ -94,17 +83,57 @@ DOVS_D %>%
 #Everything looks ok, except the Mycteroperca sp. From memory M. olfax was the only
 #species in this genus in the Galapagos. Check with Pelayo and change if needed.
 
+#Checking unique species ID's in DOVS_D and UVC
+unique(DOVS_D$SpeciesName)
+unique(UVC$SPECIES)
+
+#Correcting misspelled species names in UVC data (bonito, Paralabrax albomaclatus, yellow tail snapper,
+# Zalophus wolebacki, Hoplopagrus guenteri, Zalophus wollebackii, Zalophus wollebacki, Heterodonthus quoyi,
+# Myvteroperca olfax)
+UVC$SpeciesName <- UVC$SPECIES %>%
+  recode(., "bonito" = "Sardinops sagax", 
+         "Paralabrax albomaclatus" = "Paralabrax albomaculatus",
+         "yellow tail snapper" = "Lutjanus argentiventris",
+         "Zalophus wolebacki" = "Zalophus wollebaeki",
+         "Hoplopagrus guenteri" = "Hoplopagrus guentherii",
+         "Zalophus wollebackii" = "Zalophus wollebaeki",
+         "Zalophus wollebacki" = "Zalophus wollebaeki",
+         "Heterodonthus quoyi" = "Heterodontus quoyi",
+         "Myvteroperca olfax" = "Mycteroperca olfax")
+#Checking that the unique species are now correct in UVC
+unique(UVC$SpeciesName)
 
 
 # Biomass calculations ----------------------------------------------------
 #Getting unique species for finding a and b factors to calculate biomass
 library(data.table)
-unique_dovs <- data.frame(unique(DOVS$SPECIES))# I am not sure why one of the unique values is empty ??
-colnames(unique_dovs) <- "species"
-unique_uvc <- data.frame(unique(UVC$SPECIES))   # This one does not have an empty value..
-colnames(unique_uvc) <- "species"
-unique_species <- unique(rbind(unique_dovs, unique_uvc))
+unique_DOVS_D <- data.frame(unique(DOVS_D$SpeciesName))
+colnames(unique_DOVS_D) <- "Species"
+unique_UVC <- data.frame(unique(UVC$SpeciesName))
+colnames(unique_UVC) <- "Species"
+
+#Merging the species lists and keeping only the unique species
+species_list <- unique(rbind(unique_DOVS_D, unique_UVC))
+
+#Reading csv file with a and b factors (when I have found all a and b values)
+#calculation of biomass
 
 
-#Calculating fish biomass
+# NMDS plot for sites and species ------------------------------------------
+#Assigning columns with species names and abundance from DOVS_D and UVC
+Abun_DOVS <- subset(DOVS_D, select = c("Site", "SpeciesName", "N"))
+
+Abun_DOVS_2 <- Abun_DOVS %>%
+  group_by(Site, SpeciesName) %>%
+  mutate(Sum_N = sum(N)) %>%
+  select(-c(N)) %>%
+  mutate(Abundance, distinct(unique(Sum_N))) # This last line is not working and I do not understand why
+#There is something with the mutate and distinct maybe. 
+#It works when the line is just: distinct(unique(Sum_N))
+#But then I cannot name the column and it is just named "unique(Sum_N)" and I want it to be named "Abundance"
+
+
+#Making matrix to enable NMDS plot
+Abun_DOVS_mat <- Abun_DOVS %>%
+  pivot_wider(names_from = "SpeciesName", values_from = "Abundance") 
 
