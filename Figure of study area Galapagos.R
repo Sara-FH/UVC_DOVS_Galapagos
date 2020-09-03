ba0950eecf7c04eabe89d4be87e82069a96703ef
lidefi87 <lidefi87@hotmail.com>
2020-09-03 12:41:46 -0600

    Script corrected by DFA New shapefiles used for maps Map created with new shapefiles Sites data has been cleaned and spelling mistakes corrected. Coordinates changed to decimal degrees. Sites added to Galapagos map

--- Figure of study area Galapagos.R
+++ Figure of study area Galapagos.R
@@ -7,40 +7,47 @@
 # Loading libraries -------------------------------------------------------
 
 library(sf)
-library(ggplot2)
-library(tidyverse)
+library(tidyverse) #tidyverse includes ggplot2 so I have removed it
 library(sp)
 
+
+# Accessing data ----------------------------------------------------------
+#Loading shapefiles - DFA
+Gal_Isl <- st_read("Shapefiles/GalapagosIslands_WGS84.shp") #Galapagos Islands
+GMR <- st_read("Shapefiles/GMRBoundaries_WGS84.shp") #Boundaries of the GMR
+GMR_Zones <- st_read("Shapefiles/GMRZones_Lines_WGS84.shp") %>%  #2001 Zoning of the GMR
+  #ZONA (zoning type) will be transformed to factor as it is considered as numeric by default
+  mutate(ZONA = factor(ZONA))
+
+
 # Making the map ----------------------------------------------------------
+#Putting the map together - DFA
+MapGMR <- ggplot() + 
+  #First we plot the GMR boundary, I have set it to have a transparent fill, but you can change this under 'fill'
+  geom_sf(data = GMR, size = 1, fill = alpha("blue", 0)) + 
+  #We now plot the actual island
+  geom_sf(data = Gal_Isl, size = 0.5, color = "black", fill = "gray80") + 
+  #Finally, we will plot the GMR zones so they appear over the islands contours
+  #We will change the colour of the boundaries by zone type (ZONA)
+  geom_sf(data = GMR_Zones, aes(color = ZONA), size = 1) +
+  #I will use a colorbrewer palette for the different zones, you can change this to whatever you prefer
+  #I also changed the title of the legend
+  scale_color_brewer(palette = "Dark2", name = "Management Zones")+
+  ggtitle("Galapagos Marine Reserve Zoning") + 
+  #Applying a black and white theme to the figure
+  theme_bw()+
+  #Removing grids
+  theme(panel.grid = element_blank())
+#Checking the map looks ok
+MapGMR
 
-#Reading shapefile covering the Galapagos 
-Galapagos <- st_read("Data/worldheritagemarineprogramme.shp") #This is only the outline
-#I cannot find a shapefile with the Islands
-ggplot() + 
-  geom_sf(data = Galapagos, size = 0.5, color = "black", fill = "green") + 
-  ggtitle("Galapagos") + 
-  coord_sf()
 
-Galapagos_zones <- st_read("Data/GMR_Zoning2000_NTZ.shp") #Here I tried to get a shapefile with
-#the different zones within the GMR, but this one only has 2.2 and 2.1 zones and I have data for 
-#2.1-2.4
-ggplot() + 
-  geom_sf(data = Galapagos_zones, size = 0.5, color = "black", fill = "cyan1") + 
-  ggtitle("Galapagos_zones") + 
-  coord_sf()
-
-#Just to try out combining the maps
-ggplot() + 
-  geom_sf(data = Galapagos, size = 0.5, color = "black", fill = NA) + 
-  ggtitle("Galapagos") + 
-  coord_sf() + 
-  geom_sf(data = Galapagos_zones, size = 0.5, color = "black", fill = "cyan1") + 
-  ggtitle("Galapagos_zones") + 
-  coord_sf()
-
+# Sites sampled -----------------------------------------------------------
 #Reading data from the sites
 Sites <- read.csv2("Data/Bacalao_MagicMysteryTour_GPS_Coordinates_Respaldo.csv") %>% 
-  select(Site, Island, Bioregion, LAT, LONG, Type, Fishing, Zoning)
+  select(Site, Island, Bioregion, LAT, LONG, Type, Fishing, Zoning) %>% 
+  #I am tidying up the names so they do not look too messy
+  janitor::clean_names()
 #My microsoft package has been updated, so now it uses comma (,) instead of a dot (.)
 #That's why I am using read.csv2.
 str(Sites)
@@ -49,11 +56,47 @@ str(Sites)
 #it is also in characters, but it reads the entire cell as 1, not just the first letter, I was
 #trying to move N to the back, to try to rearrange to a format that could be changed to 
 #DD (decimal degrees).
+
+#DFA responds: When I load the dataset it shows lat and long as factors and they come up as, for example:
+#N1° 40.320', W91° 59.361'. So I am not exactly sure what the error is.
+
+
 #Overall I am trying to make the current format into decimal degrees so I can plot it on a map
 #hereby, putting the sites on a map of the Galapagos
 
-Sites$LAT2 <- Sites$LAT 
-  toString(print(Sites$LAT[1] + Sites$LAT[0]))
+#DFA responds: I am not sure what you tried to do before, but I am changing the degrees decimal minutes to
+#decimal degrees
 
-Sites$LAT[1]
+#First, we need to change the lat and long columns from factors to characters - DFA
+Sites <- Sites %>% mutate(lat = as.character(lat), 
+                 long = as.character(long),
+                 #I am also correcting that typo under the column 'type' from turism to tourism
+                 #We need to convert the column from factor to character
+                 type = as.character(type),
+                 #Then we correct the mistake
+                 type = case_when(type == "Turism" ~ "Tourism",
+                                   TRUE ~ type),
+                 #Finally we change the column back to factor
+                 type = factor(type))
+
+#We will call a library called parzer (I mentioned this in a previous email, check emails for more information) - DFA
+library(parzer)
+
+#New columns created so you can check coordinates are correctly transformed - DFA
+Sites <- Sites %>% mutate(latDD = parse_lat(lat),
+                 lonDD = parse_lon(long))
+#You can check coords have been converted correctly using this website: https://www.pgc.umn.edu/apps/convert/
+
+#If you want to show these points on the map we previously created, we first need to create a shapefile - DFA
+SiteShp <- Sites %>% 
+  #We use the newly created lat and lon columns as coordinates
+  st_as_sf(coords = c("lonDD", "latDD"), 
+           #We assume the CRS for these coordinates is WGS84, because it uses lat and lon and it is commonly used in 
+           #the Galapagos
+           crs = 4326)
+
+#Now we simply use the previous map we created and add our data - DFA
+MapGMR+
+  #I decreased the size of the dots and made them slightly transparent so we can see the management zones clearly
+  geom_sf(data = SiteShp, size = 1, colour = alpha("#882255", 0.6))
 
