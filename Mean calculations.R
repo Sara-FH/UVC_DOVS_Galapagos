b81a4a28b8944899fe83dc3f2bfb917ec9b6416c
Sara-FH <faerch23@gmail.com>
2021-01-15 17:32:51 +0100

    Mean calculations made for species of interests for mean densities and biomasses.

--- Mean calculations.R
+++ Mean calculations.R
@@ -209,3 +209,124 @@ sp_bio_PCO <- Biomass_sp %>%
 
 
 rm(sp_den_PCO, sp_bio_PCO)
+
+
+# Mean for species biomass and density Figure B and D ---------------------
+
+#Calculating biomass for DOVS
+Bio_sp_D <- DOVS %>%
+  mutate(LenLenRatio = as.numeric(LenLenRatio)) %>% 
+  #The fish biomass equation is W = a*L^b, therefore first transform the length, 
+  #then apply the exponent b and finally multiply by a.
+  #The equation is therefore: a*((LenLenRatio*Length_cm)^b)
+  mutate(Biomass = a*((LenLenRatio*Length_cm)^b)) %>% #Biomass for 1 individual of each species
+  mutate(Biomass_N = Biomass*N) %>% #Biomass for all individuals of each species
+  group_by(Site, Period, ValidName) %>% 
+  #Summing biomass for each species in each period, so I have total biomass per species per period
+  summarise(Biomass_sp_period = sum(Biomass_N), 
+            Transect_length_m, Method, Fishing, SiteCode, Period) %>% 
+  unique() %>% 
+  mutate(Transect_area = Transect_length_m*5) %>% #Calculating transect area using transect width 5m
+  group_by(Site, ValidName) %>% 
+  #Calculating gram per 500m2
+  summarise(Gram_500m2 = (Biomass_sp_period/Transect_area)*500, 
+            Method, Fishing, SiteCode, Biomass_sp_period, Transect_area, Period) %>% 
+  #Calculating the biomass of each species per site in g per 500m2 (as average of periods)
+  summarise(Gram_site_sp = mean(Gram_500m2), 
+            ValidName, Method, Fishing, SiteCode, Biomass_sp_period, Transect_area, Period) %>%
+  #Going from g to kg for the biomass
+  mutate(Kg_site_sp = Gram_site_sp/1000) %>% #Biomass in kg
+  select(-Gram_site_sp) %>% #Removing the Gram_site_sp column
+  #Making Biomass_sp_period into kg from g
+  mutate(Biomass_sp_period = Biomass_sp_period/1000) %>% 
+  #Calculating biomass per period per 500m2
+  mutate(Kg_period_500m2 = (Biomass_sp_period/Transect_area)*500) %>% 
+  group_by(Site, ValidName) %>% 
+  #Calculating standard error
+  mutate(SE_B = sd(Kg_period_500m2)/sqrt(length(Kg_period_500m2))) %>% 
+  ungroup()
+
+#Calculating biomass UVC
+Bio_sp_U <- UVC %>% 
+  #The fish biomass equation is W = a*L^b, therefore first transform the length, 
+  #then apply the exponent b and finally multiply by a.
+  #The equation is therefore: a*((LenLenRatio*Length_cm)^b)
+  mutate(Biomass = a*((TLRatio*Length_cm)^b)) %>% #Biomass for 1 individual of each species
+  mutate(Biomass_N = Biomass*N) %>% #Biomass for all individuals of each species
+  group_by(Site, Period, ValidName) %>% 
+  #Summing biomass for each species in each period, so I have total biomass per species per site
+  summarise(Biomass_sp_period = sum(Biomass_N), 
+            Transect_length_m, Method, Fishing, SiteCode, Period) %>% 
+  unique() %>% 
+  mutate(Transect_area = Transect_length_m*5) %>% #Calculating transect area using transect width 5m
+  group_by(Site, ValidName) %>% 
+  #Calculating gram per 500m2
+  summarise(Gram_500m2 = (Biomass_sp_period/Transect_area)*500, 
+            Method, Fishing, SiteCode, Biomass_sp_period, Transect_area, Period) %>% 
+  #Calculating the biomass of each species per site in g per 500m2 (as average of periods)
+  summarise(Gram_site_sp = mean(Gram_500m2), 
+            ValidName, Method, Fishing, SiteCode, Biomass_sp_period, Transect_area, Period) %>%
+  #Going from g to kg for the biomass
+  mutate(Kg_site_sp = Gram_site_sp/1000) %>% #Biomass in kg
+  select(-Gram_site_sp) %>% #Removing the Gram_site_sp column
+  #Making Biomass_sp_period into kg from g
+  mutate(Biomass_sp_period = Biomass_sp_period/1000) %>% 
+  #Calculating biomass per period per 500m2
+  mutate(Kg_period_500m2 = (Biomass_sp_period/Transect_area)*500) %>% 
+  group_by(Site, ValidName) %>% 
+  #Calculating standard error
+  mutate(SE_B = sd(Kg_period_500m2)/sqrt(length(Kg_period_500m2))) %>% 
+  ungroup()
+
+#Combine biomass per species data frames
+Bio_sp <- rbind(Bio_sp_D, Bio_sp_U) %>% 
+  #removing columns used to calculate SE
+  select(-c(Biomass_sp_period, Transect_area, Period, Kg_period_500m2)) %>% 
+  unique()
+
+#Removing unnecessary variables
+rm(Bio_sp_D, Bio_sp_U)
+
+
+#Calculating average density of each species per site for DOVS
+Den_sp_D <- DOVS %>% 
+  select(Site, Period, N, Method, ValidName, Fishing, SiteCode, Transect_length_m) %>% 
+  mutate(Transect_area = Transect_length_m*5) %>%  #calculating transect area by multiplying with width 5m
+  group_by(Site, Period, ValidName) %>% 
+  summarise(N_period = sum(N), #Sum of abundance of species within each period
+            Method, Fishing, SiteCode, Transect_area, Period) %>% 
+  unique() %>%
+  mutate(N_500m2 = (N_period/Transect_area)*500) %>% #Number/500m2
+  group_by(Site, ValidName) %>% 
+  summarise(N_site_sp = mean(N_500m2), #Calculating average abundance of each species per site
+            Method, Fishing, SiteCode, ValidName, N_500m2, Transect_area, Period) %>% 
+  group_by(Site, ValidName) %>% 
+  #Calculating standard error
+  mutate(SE_D = sd(N_500m2)/sqrt(length(N_500m2))) %>% 
+  ungroup()
+
+
+#Calculating average density of each species per site for UVC
+Den_sp_U <- UVC %>% 
+  select(Site, Period, N, Method, ValidName, Fishing, SiteCode, Transect_length_m) %>% 
+  mutate(Transect_area = Transect_length_m*5) %>%  #calculating transect area by multiplying with width 5m
+  group_by(Site, Period, ValidName) %>% 
+  summarise(N_period = sum(N), #Sum of abundance of species within each period
+            Method, Fishing, SiteCode, Transect_area, Period) %>% 
+  unique() %>%
+  mutate(N_500m2 = (N_period/Transect_area)*500) %>% #Number/500m2
+  group_by(Site, ValidName) %>% 
+  summarise(N_site_sp = mean(N_500m2), #Calculating average abundance of each species per site
+            Method, Fishing, SiteCode, ValidName, N_500m2, Transect_area, Period) %>% 
+  group_by(Site, ValidName) %>% 
+  #Calculating standard error
+  mutate(SE_D = sd(N_500m2)/sqrt(length(N_500m2))) %>% 
+  ungroup()
+
+#Combining average density for DOVS and UVC and making a matrix
+Den_sp <- rbind(Den_sp_D, Den_sp_U) %>% 
+  select(-c(N_500m2, Transect_area, Period))
+
+rm(Den_sp_D, Den_sp_U)
+
+
